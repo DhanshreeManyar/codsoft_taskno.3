f928b1d04e950f1753ab1b51ca8c3dbcfe6b9b46
DhanshreeManyar <98585939+DhanshreeManyar@users.noreply.github.com>
2023-10-27 20:16:24 +0530

    Add files via upload

--- codsoft_taskno.03.py
+++ codsoft_taskno.03.py
@@ -1,44 +1,97 @@
-from tkinter import *
+
 import random
-import string
 import pyperclip
+from tkinter import *
+from tkinter.ttk import *
 
+def low():
+	entry.delete(0, END)
+	length = var1.get()
+
+	lower = "abcdefghijklmnopqrstuvwxyz"
+	upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	digits = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 !@#$%^&*()"
+	password = ""
+
+	if var.get() == 1:
+		for i in range(0, length):
+			password = password + random.choice(lower)
+		return password
+
+	# if strength selected is medium
+	elif var.get() == 0:
+		for i in range(0, length):
+			password = password + random.choice(upper)
+		return password
+
+	# if strength selected is strong
+	elif var.get() == 3:
+		for i in range(0, length):
+			password = password + random.choice(digits)
+		return password
+	else:
+		print("Please choose an option")
+
+
+# Function for generation of password
+def generate():
+	password1 = low()
+	entry.insert(10, password1)
+
+
+# Function for copying password to clipboard
+def copy1():
+	random_password = entry.get()
+	pyperclip.copy(random_password)
+
+
+# Main Function
+
+# create GUI window
 root = Tk()
-root.title("Password Generator App")
-root.geometry("650x500+250+100")
-root.config(bg="darkturquoise")
-f = ("Arial", 20, "bold")
+var = IntVar()
+var1 = IntVar()
 
-def generate_password():
-	password_length = int(length_entry.get())
-	if password_length < 2:
-		password_label.config(text="Password length must be at least 2 characters")
-		return
+# Title of your GUI window
+root.title("Random Password Generator")
 
-	characters = string.ascii_letters + string.digits + string.punctuation
-	password = ''.join(random.choice(characters) for _ in range(password_length))
-	password_label.config(text="Generated Password: " + password)
-	copy_button.config(state="normal")
+# create label and entry to show
+# password generated
+Random_password = Label(root, text="Password")
+Random_password.grid(row=0)
+entry = Entry(root)
+entry.grid(row=0, column=1)
 
-lab_header = Label(root, text = "Random Password generator ", font=f)
-lab_header.pack(pady=30)
+# create label for length of password
+c_label = Label(root, text="Length")
+c_label.grid(row=1)
 
-length_label = Label(root, text="Enter Password Length:", font=f)
-length_label.pack()
-length_entry = Entry(root, font = f)
-length_entry.pack(pady=20)
+# create Buttons Copy which will copy
+# password to clipboard and Generate
+# which will generate the password
+copy_button = Button(root, text="Copy", command=copy1)
+copy_button.grid(row=0, column=2)
+generate_button = Button(root, text="Generate", command=generate)
+generate_button.grid(row=0, column=3)
 
-generate_button = Button(root, text="Generate Password", font=f, command=generate_password, bg = "lawngreen")
-generate_button.pack()
+# Radio Buttons for deciding the
+# strength of password
+# Default strength is Medium
+radio_low = Radiobutton(root, text="Low", variable=var, value=1)
+radio_low.grid(row=1, column=2, sticky='E')
+radio_middle = Radiobutton(root, text="Medium", variable=var, value=0)
+radio_middle.grid(row=1, column=3, sticky='E')
+radio_strong = Radiobutton(root, text="Strong", variable=var, value=3)
+radio_strong.grid(row=1, column=4, sticky='E')
+combo = Combobox(root, textvariable=var1)
 
-password_label = Label(root, text="", font=f)
-password_label.pack(pady=20)
+# Combo Box for length of your password
+combo['values'] = (8, 9, 10, 11, 12, 13, 14, 15, 16,
+				17, 18, 19, 20, 21, 22, 23, 24, 25,
+				26, 27, 28, 29, 30, 31, 32, "Length")
+combo.current(0)
+combo.bind('<<ComboboxSelected>>')
+combo.grid(column=1, row=1)
 
-def copy_to_clipboard():
-	generated_password = password_label.cget("text")[20:]
-	pyperclip.copy(generated_password)
-
-copy_button = Button(root, text="Copy Password", font=f, command=copy_to_clipboard, bg="lawngreen", state="disabled")
-copy_button.pack(pady=20)
-
+# start the GUI
 root.mainloop()
